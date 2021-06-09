afce40278828e68b4bb70f72c32ac9ab8a4ae758
CaotiHazard <guilsalomao@gmail.com>
2021-06-09 15:46:49 -0300

    commit inicial

---
+++ src/components/about_me/about.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './about.css'
+const About = () => {
+    return (
+        <div class="about_div">
+            <div class="about_text">
+                <h2>
+                    Quem sou eu?
+                 </h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget diam vulputate, commodo ante porta, efficitur nisl. Etiam a commodo orci. Nunc sagittis aliquam varius. Suspendisse potenti. Sed vel ornare mauris. Duis vitae mollis ante, at molestie leo. Mauris ullamcorper turpis in neque maximus, ut viverra lectus ultrices. Suspendisse lacinia hendrerit dictum. Suspendisse varius lacinia leo, sit amet maximus turpis fringilla in. Nunc ornare luctus lorem. Pellentesque a tortor rhoncus, ultricies leo eget, dapibus eros. Etiam ornare imperdiet gravida. Aliquam augue libero, convallis at volutpat scelerisque, mollis quis massa. Nulla et sapien semper, mollis odio tempus, facilisis metus. Pellentesque pulvinar laoreet elementum.
+                </p>
+                <h2>
+                    Meus interesses?
+                 </h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget diam vulputate, commodo ante porta, efficitur nisl. Etiam a commodo orci. Nunc sagittis aliquam varius. Suspendisse potenti. Sed vel ornare mauris. Duis vitae mollis ante, at molestie leo. Mauris ullamcorper turpis in neque maximus, ut viverra lectus ultrices. Suspendisse lacinia hendrerit dictum. Suspendisse varius lacinia leo, sit amet maximus turpis fringilla in. Nunc ornare luctus lorem. Pellentesque a tortor rhoncus, ultricies leo eget, dapibus eros. Etiam ornare imperdiet gravida. Aliquam augue libero, convallis at volutpat scelerisque, mollis quis massa. Nulla et sapien semper, mollis odio tempus, facilisis metus. Pellentesque pulvinar laoreet elementum.
+                </p>
+            </div>
+
+        </div>
+    )
+}
+
+export default About
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './fonts.css';
+
+
+import Landing from './components/landing/landing'
+import About from './components/about_me/about'
+
+
+class Page extends React.Component{
+    render(){
+        return (
+            <div>
+               
+                <Landing/>
+                <About/>
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <Page />,
+    document.getElementById('root')
+  );
